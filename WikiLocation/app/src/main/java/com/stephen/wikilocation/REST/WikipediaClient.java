cd5927ae53a3aad49e556149e3d21c1520780ae3
Stephen Jin <Stephen Jin>
2017-03-14 21:14:06 -0400

    defined the REST client using retrofit specifications

--- WikiLocation/app/src/main/java/com/stephen/wikilocation/REST/WikipediaClient.java
+++ WikiLocation/app/src/main/java/com/stephen/wikilocation/REST/WikipediaClient.java
@@ -1,6 +1,21 @@
 package com.stephen.wikilocation.REST;
 
+import com.stephen.wikilocation.Model.Article;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+
 public interface WikipediaClient {
-       
+
+   @GET("")
+   Call<List<Article>> getArticlesNearby(
+           @Query("action") String action,
+           @Query("list") String geoSearch,
+           @Query("gsradius") int radius,
+           @Query("gscoord") String cordinates
+   );
 }
 
