7a58fe18c67d1782c5d1b8fa9f16e4b048a62b8f
Stephen Jin <Stephen Jin>
2017-03-14 21:40:27 -0400

    added code for the service generator that generates a retrofit object to be used with the REST client

--- WikiLocation/app/src/main/java/com/stephen/wikilocation/REST/ServiceGenerator.java
+++ WikiLocation/app/src/main/java/com/stephen/wikilocation/REST/ServiceGenerator.java
@@ -1,7 +1,30 @@
 package com.stephen.wikilocation.REST;
 
+import com.stephen.wikilocation.R;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Stephen on 3/14/2017.
  */
 public class ServiceGenerator {
+    private static final String BASE_URL = "https://en.wikipedia.org/w/api.php";
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    private static OkHttpClient.Builder httpClient =
+            new OkHttpClient.Builder();
+
+    public static <S> S createService(
+            Class<S> serviceClass) {
+        return retrofit.create(serviceClass);
+    }
+
 }
